3a2fec2c6138ee803d1f6c5d13bb363fb3b5ace3
Medard <medardkougnaglo@gmail.com>
2024-05-22 14:51:35 +0000

    la premiere version de mon code html de la créatio  du site UL

---
+++ script.js
@@ -0,0 +1,19 @@
+// const ratio = .1
+// const options = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: ratio,
+// };
+
+// const handleIntersect = function(entries, observer) {
+//     entries.forEach(function(entry){
+//         if(entry.intersectionRatio > ratio){
+//             entry.target.classList.add('reveal-visible')
+//             observer.unObserve(entry.target)
+//     }   
+//     })
+ 
+// }   
+ 
+// // const observer = new IntersectionObserver(handleIntersect, options);
+// // observer.observe(document.querySelector('.reveal'))
